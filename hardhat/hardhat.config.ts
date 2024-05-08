654a905853ec6efc80fd95c1703ab823943c36dd
Layr <layrfpf@gmail.com>
2024-05-08 20:41:17 -0300

    setting up to deploy contract

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -5,6 +5,11 @@ import '@nomiclabs/hardhat-ethers'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    }
+  },
 };
 
 export default config;
