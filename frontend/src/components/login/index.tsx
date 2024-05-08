f1ffab582be460e46ae99c6e45d1517301480f95
Layr <layrfpf@gmail.com>
2024-05-08 19:12:42 -0300

    first commit

---
+++ frontend/src/components/login/index.tsx
@@ -0,0 +1,36 @@
+import styles from "@/styles/Login.module.scss"
+import Link from "next/link"
+
+export default function Login () {
+    return(
+        <div className={styles.loginWrapper}>
+            <div className={styles.oldUserLoginFormWrapper}>
+                <div className={styles.inputWrapper}>
+                    <span>CPF</span>
+                    <input placeholder="Digite seu CPF"></input>
+                </div>
+
+                <div className={styles.inputWrapper}>
+                    <span>Matrícula de sócio</span>
+                    <input placeholder="Digite aqui sua matrícula"></input>
+                </div>
+
+                <button>Entrar</button>
+            </div>
+
+            <div className={styles.newUserLoginFormWrapper}>
+                <h2>Primeiro acesso?</h2>
+                <h3>Digite seu CPF e confira os drops disponíveis para você</h3>
+                
+                <div className={styles.inputWrapper}>
+                    <span>CPF</span>
+                    <input placeholder="Digite seu CPF"></input>
+                </div>
+
+                <Link href="/myDrops" className={styles.link}>
+                    <button id={styles.seeDropsBtn}>Ver drops</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
