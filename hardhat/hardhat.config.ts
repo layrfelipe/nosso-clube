3e1f4bf986fbdf127a136c903842e327cc9a7207
Layr <layrfpf@gmail.com>
2024-05-08 21:49:05 -0300

    passou nos testesssss

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,10 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
-import '@nomiclabs/hardhat-ethers'
-
+import "@nomiclabs/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      // You can specify chainId here if you need to match an Ethereum testnet or mainnet
+      chainId: 1337,
+      // If you require more detailed logging:
+      loggingEnabled: true
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
 
 export default config;
